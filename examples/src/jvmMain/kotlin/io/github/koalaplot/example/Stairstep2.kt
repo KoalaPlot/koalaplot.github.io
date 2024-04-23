12a0fa6734692eb84f84e83fb1dcb7dc854fd166
Greg Steckman <27028452+gsteckman@users.noreply.github.com>
2024-04-23 08:02:56 -0700

    Updates for 0.6.0

---
+++ examples/src/jvmMain/kotlin/io/github/koalaplot/example/Stairstep2.kt
@@ -0,0 +1,32 @@
+package io.github.koalaplot.example
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.window.singleWindowApplication
+import io.github.koalaplot.core.line.StairstepPlot
+import io.github.koalaplot.core.style.LineStyle
+import io.github.koalaplot.core.util.ExperimentalKoalaPlotApi
+import io.github.koalaplot.core.xygraph.DefaultPoint
+import io.github.koalaplot.core.xygraph.XYGraph
+import io.github.koalaplot.core.xygraph.autoScaleXRange
+import io.github.koalaplot.core.xygraph.autoScaleYRange
+import io.github.koalaplot.core.xygraph.rememberFloatLinearAxisModel
+
+@OptIn(ExperimentalKoalaPlotApi::class)
+fun main() = singleWindowApplication {
+    val data = buildList {
+        for (i in 1..10) {
+            add(DefaultPoint(i.toFloat(), i * i.toFloat()))
+        }
+    }
+
+    XYGraph(
+        rememberFloatLinearAxisModel(data.autoScaleXRange()),
+        rememberFloatLinearAxisModel(data.autoScaleYRange())
+    ) {
+        StairstepPlot(
+            data,
+            lineStyle = LineStyle(SolidColor(Color.Blue))
+        )
+    }
+}
